008cd8df9d136dd84cff8aa800b4a97f5e2eb226
martin-ngigi <martinwainaina001@gmail.com>
2023-11-20 13:33:02 +0300

    Initial commit

---
+++ AirBnB/AirBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBApp.swift
+//  AirBnB
+//
+//  Created by Martin Wainaina on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
