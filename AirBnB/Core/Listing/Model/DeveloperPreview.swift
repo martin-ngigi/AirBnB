963ca7ce1568f3cead589c43405302f3e231d383
martin-ngigi <martinwainaina001@gmail.com>
2023-11-22 15:06:47 +0300

    Added models

---
+++ AirBnB/Core/Listing/Model/DeveloperPreview.swift
@@ -0,0 +1,134 @@
+//
+//  DeveloperPreview.swift
+//  AirBnB
+//
+//  Created by Martin Wainaina on 22/11/2023.
+//
+
+import Foundation
+
+struct DeveloperPreview{
+    var listings: [Listing] = [
+        .init(
+            id: NSUUID().uuidString,
+            owenerUid:  NSUUID().uuidString,
+            owenerName: "John Smith",
+            owenerImageImageUrl: "default",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 2,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 100,
+            latitude: -1.2920659,
+            longitude: 36.82194619,
+            imageUrls: [
+                "bedroom2",
+                "bathroom",
+                "bedroom1",
+                "front",
+                "kitchen",
+                "living_room",
+                "master_bedroom"
+            ],
+            address: "Kileleshwa, Nairobi, Kenya",
+            city: "Nairobi",
+            state: "Nairobi",
+            title: "Nairobi Kenya",
+            rating: 4.3,
+            features: [.selfCheckin, .superHost],
+            amenities: [.wifi, .tv, .pool, .balcony],
+            type: .villa
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            owenerUid:  NSUUID().uuidString,
+            owenerName: "John Smith",
+            owenerImageImageUrl: "default",
+            numberOfBedrooms: 3,
+            numberOfBathrooms: 1,
+            numberOfGuests: 3,
+            numberOfBeds: 2,
+            pricePerNight: 90,
+            latitude: -1.2820659,
+            longitude: 36.72194619,
+            imageUrls: [
+                "front",
+                "bathroom",
+                "bedroom1",
+                "bedroom2",
+                "kitchen",
+                "living_room",
+                "master_bedroom"
+            ],
+            address: "Muthaiga, Nairobi, Kenya",
+            city: "Nairobi",
+            state: "Nairobi",
+            title: "Nairobi Kenya",
+            rating: 3.9,
+            features: [.selfCheckin],
+            amenities: [.tv, .pool, .balcony],
+            type: .villa
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            owenerUid:  NSUUID().uuidString,
+            owenerName: "John Smith",
+            owenerImageImageUrl: "default",
+            numberOfBedrooms: 4,
+            numberOfBathrooms: 3,
+            numberOfGuests: 4,
+            numberOfBeds: 4,
+            pricePerNight: 120,
+            latitude: -1.3720659,
+            longitude: 36.84194619,
+            imageUrls: [
+                "master_bedroom"
+                "bathroom",
+                "bedroom1",
+                "bedroom2",
+                "front",
+                "kitchen",
+                "living_room",
+            ],
+            address: "Lavington, Nairobi, Kenya",
+            city: "Nairobi",
+            state: "Nairobi",
+            title: "Nairobi Kenya",
+            rating: 4.7,
+            features: [.selfCheckin, .superHost],
+            amenities: [.wifi, .alarmSystem, .tv, .pool, .balcony],
+            type: .villa
+        ),
+        
+            .init(
+                id: NSUUID().uuidString,
+                owenerUid:  NSUUID().uuidString,
+                owenerName: "John Smith",
+                owenerImageImageUrl: "default",
+                numberOfBedrooms: 4,
+                numberOfBathrooms: 3,
+                numberOfGuests: 4,
+                numberOfBeds: 4,
+                pricePerNight: 120,
+                latitude: -1.3720659,
+                longitude: 36.84194619,
+                imageUrls: [
+                    "kitchen",
+                    "bathroom",
+                    "bedroom1",
+                    "bedroom2",
+                    "front",
+                    "living_room",
+                    "master_bedroom"
+                ],
+                address: "Lavington, Nairobi, Kenya",
+                city: "Nairobi",
+                state: "Nairobi",
+                title: "Nairobi Kenya",
+                rating: 4.7,
+                features: [.selfCheckin, .superHost],
+                amenities: [.wifi, .alarmSystem, .tv, .pool, .balcony],
+                type: .villa
+            ),
+    ]
+}
---
+++ AirBnB/Core/Listing/Model/Listing.swift
@@ -0,0 +1,119 @@
+//
+//  Listing.swift
+//  AirBnB
+//
+//  Created by Martin Wainaina on 22/11/2023.
+//
+
+import Foundation
+
+struct Listing: Identifiable, Codable{
+    let id: String
+    let owenerUid: String
+    let owenerName: String
+    let owenerImageImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    let pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let imageUrls: [String]
+    let address: String
+    let city: String
+    let state: String
+    let title: String
+    let rating: Double
+    let features: [ListingFeatures]
+    let amenities: [ListingAmenities]
+    let type: ListingType
+}
+
+enum ListingFeatures: Int, Codable, Identifiable, Hashable{
+    case selfCheckin
+    case superHost
+    
+    var imageName: String {
+        switch self{
+        case .selfCheckin: return "door.left.hand.open"
+        case .superHost: return "modal"
+        }
+    }
+    
+    var title: String{
+        switch self{
+        case .selfCheckin: return "Self Checkin"
+        case .superHost: return "SuperHost"
+        }
+    }
+    
+    var subTitle: String{
+        switch self{
+        case .selfCheckin: return "Check yourself with the keypad"
+        case .superHost: return "Superhost are experinced, highly rated who are commited to providong greate stars for guests"
+        }
+    }
+    
+    var id: Int { return self.rawValue}
+}
+
+enum ListingAmenities: Int, Codable, Identifiable, Hashable{
+    case pool
+    case kitchen
+    case wifi
+    case laundry
+    case tv
+    case alarmSystem
+    case office
+    case balcony
+    
+    var title: String{
+        switch self{
+        case .pool: return "Pool"
+        case .kitchen: return "Kitchen"
+        case .wifi: return "WiFi"
+        case .laundry: return "Laundry"
+        case .tv: return "TV"
+        case .alarmSystem: return "Alarm System"
+        case .office: return "Office"
+        case .balcony: return "Balcony"
+        }
+    }
+    
+    var imageName: String{
+        switch self{
+        case .pool: return "figure.pool.swim"
+        case .kitchen: return "fork.knife"
+        case .wifi: return "wifi"
+        case .laundry: return "washer"
+        case .tv: return "tv"
+        case .alarmSystem: return "checkerboard.shield"
+        case .office: return "pencil.and.ruler.fill"
+        case .balcony: return "building"
+        }
+    }
+    
+    var id: Int { return self.rawValue}
+}
+
+enum ListingType: Int, Codable, Identifiable, Hashable{
+    case apartment
+    case house
+    case townHouse
+    case villa
+    
+    var description: String{
+        switch self{
+        case .apartment: return "Apartment"
+        case .house: return "House"
+        case .townHouse: return "Town House"
+        case .villa: return "Villa"
+        }
+    }
+    
+    var id: Int { return self.rawValue}
+
+}
+
+
